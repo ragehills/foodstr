91ee947ceacfe70b60b796d3841b553a94e72095
Rage Hills <1207581brunel@gmail.com>
2017-02-15 17:33:32 +0000

    routes.js linked to app.js

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 var express = require("express");
 var app = express();
+var port = process.env.PORT || 3000;
+var router = require('./config/routes');
+
+
+
+
+app.use(router);
+
+app.listen(port, function() {
+	console.log("The server is on and listening on port " + port);
+});
+
+
+ 	
---
+++ config/routes.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+var carsController = require('../controllers/recipes');
+
+router.route('/')
+  .get(recipesController.index)
+  .post(recipesController.create);
+	
+router.get('/new', recipesController.new);
+	
+router.route('/:id')
+  .get(recipesController.show)
+  .put(recipesController.update)
+  .delete(recipesController.delete);
+	
+router.get('/:id/edit', recipesController.edit);
+ 	
+module.exports = router;
