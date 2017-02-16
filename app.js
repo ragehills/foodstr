3266b5e5710119bb2e9f4b760e2b540a8b9ed0de
Rage Hills <1207581brunel@gmail.com>
2017-02-16 11:52:16 +0000

    npm ejs and ejs layouts installed layout.ejs populated with html template model updated

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ var app = express();
 var port = process.env.PORT || 3000;
 var router = require('./config/routes');
 var mongoose = require('mongoose');
+var layouts = require('express-ejs-layouts');
 
 
 mongoose.connect('mongodb://localhost/recipes', function() {
 	console.log('database connected.')
 })
 
+
+
+
+
+
+express.set('view engine', 'ejs');
+app.use(layouts);
+
+
 app.use(router);
 
 app.listen(port, function() {
 	console.log("The server is on and listening on port " + port);
 });
-
-
  	
 module.exports = app
--- models/recipe.js
+++ models/recipe.js
@@ -1,13 +1,35 @@
 var mongoose = require('mongoose');
 
 var RecipeSchema = mongoose.Schema({
-  title: String,
-  ingredient: String,
-  method: String,
-  cookTime: Number,
-  prepTime: Number,
-  serves: Number,
-  skill: Number
+  title: {
+  	type: String,
+  	required: true,
+  	unique: true,
+  },
+  ingredient: {
+  	type: String,
+  	required: true
+  },
+  method: {
+  	type: String,
+  	required: true
+  },
+  cookTime: {
+  	type: Number,
+  	required: true
+  },
+  prepTime: {
+  	type: Number,
+  	required: true
+  },
+  serves: {
+  	type: Number,
+  	required: true
+  },
+  skill: {
+  	type: Number,
+  	required: true
+  }
 })
 
 module.exports = mongoose.model('Recipe', RecipeSchema);
