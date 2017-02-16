052832f65fe6cc69cc4263021299dc98029d8b85
Rage Hills <1207581brunel@gmail.com>
2017-02-16 15:00:55 +0000

    2 tests working now, create, show needs updating

--- views/recipes/form.js
+++ views/recipes/form.js
@@ -1,15 +1,28 @@
 <form action="/<%= recipe.id %>" method="POST">
-  <input type="text" name="title" placeholder="Enter a title" >
-  <textarea name="ingredient" placeholder="Ingredients"></textarea>
-  <textarea name="method" placeholder="Method"><%= recipe.body %></textarea>
-  <input type="number" name="cookTime" min="0">
-  <input type="number" name="prepTime" min="0">
-  <input type="number" name="serves" min="0" max="10">
-  <input type="number" name="difficulty" min="0" max="5">
+	<label for="title">Title</label>
+	<input type="text" name="title" placeholder="Enter a title" >
 
-  <input type="submit" value="Save recipe">
-  <% if(recipe.id != "") { %>
-  <input type="hidden" name="_method" value="PUT">
-  <% } %>
+	<label for="ingredient">Ingredient</label>
+	<textarea name="ingredient" placeholder="Ingredients"></textarea>
+
+	<label for="method">Method</label>
+	<textarea name="method" placeholder="Method"><%= recipe.body %></textarea>
+
+	<label for="cookTime">Cook Time:</label>
+	<input type="number" name="cookTime" min="0">
+
+	<label for="prepTime">Prep Time:</label>
+	<input type="number" name="prepTime" min="0">
+
+	<label for="serves">Serves</label>
+	<input type="number" name="serves" min="0" max="10">
+
+	<label for="difficulty">Difficulty Level</label>
+	<input type="number" name="difficulty" min="0" max="5">
+
+	<input type="submit" value="Save recipe">
+	<% if(recipe.id != "") { %>
+	<input type="hidden" name="_method" value="PUT">
+	<% } %>
 </form>
 
